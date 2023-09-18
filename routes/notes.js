24f5079a2ef2591d8d822937237c3f23eaf20a94
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2023-09-18 18:58:11 -0400

    POST Request

--- routes/notes.js
+++ routes/notes.js
@@ -13,8 +13,20 @@ note.get('/api/notes', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
-// POST route for adding a new note
-
+// POST route for adding a new note as json data
+note.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        let newNote = req.body;
+        newNote.id = notes.length + 1;
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+});
 // DELETE route for deleting a note
 
 module.exports = note;
